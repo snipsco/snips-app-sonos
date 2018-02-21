bf0860d4612bbc1be0ec3c11f38f4fa18f5513b3
Bjay Watanabe Kamwa <bjay.kamwa-watanabe@snips.ai>
2018-02-21 16:30:46 +0000

    Add play sonos Playlist

--- snipssonos/provider/local_player.py
+++ snipssonos/provider/local_player.py
@@ -25,13 +25,33 @@ class LocalPlayer(ProviderPlayerTemplate):
         return True
 
     def play_track(self, device, name, shuffle=False):
-        return self.play(device, name, shuffle, self.my_library.get_tracks)
+        return self.play(device, name, shuffle, self.my_library.get_tracks) or\
+                self.play(device, name, shuffle, device.music_library.get_tracks)
 
     def play_artist(self, device, name, shuffle=False):
-        return self.play(device, name, shuffle, self.my_library.get_artists)
+        return self.play(device, name, shuffle, self.my_library.get_artists) or\
+                self.play(device, name, shuffle, device.music_library.get_artists)
+
 
     def play_album(self, device, name, shuffle=False):
-        return self.play(device, name, shuffle, self.my_library.get_albums)
+        return self.play(device, name, shuffle, self.my_library.get_albums) or\
+                self.play(device, name, shuffle, device.music_library.get_albums)
 
     def play_playlist(self, device, name, shuffle=False):
-        return self.play(device, name, shuffle, self.my_library.get_playlists)
+        return self.play(device, name, shuffle, self.my_library.get_playlists)\
+                or self.play_sonos_playlist(device, name, shuffle)
+
+    def play_sonos_playlist(self, device, name, shuffle=False):
+        playlists =list(device.get_sonos_playlists())
+        if not playlists:
+            return False
+        for playlist in playlists:
+            if (playlist.title == name):
+                device.stop()
+                device.clear_queue()
+                device.add_to_queue(playlist)
+                if shuffle:
+                    device.play_mode("SHUFFLE_NOREPEAT")
+                device.play_from_queue(0)
+                return True
+        return False
