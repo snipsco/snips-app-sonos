800254a3c220122693530abc77560ce22819e580
anthonyray <dantho361@hotmail.com>
2018-05-22 16:48:46 +0200

    adds exceptions

--- snipssonos/exceptions.py
+++ snipssonos/exceptions.py
@@ -1,9 +1,22 @@
-class SonosClientException(Exception):
-    """An exception occured the Sonos client."""
+class SonosActionException(Exception):
+    """An exception occured with the Sonos Action."""
+
+
+class ServiceException(SonosActionException):
+    """An exception occured within a service"""
+
+
+class DeviceDiscoveryException(ServiceException):
+    """An exceptio occured with the device discovery service"""
+
+
+class NoReachableDeviceException(DeviceDiscoveryException):
+    """No connected devices were found by the DeviceDiscovery service"""
 
 
 class APIRequestError(Exception):
     """An exception occured when interacting with Sonos API."""
+
 
 class APIRequestWrongParams(APIRequestError):
     """The API was called with wrong parameters. """
