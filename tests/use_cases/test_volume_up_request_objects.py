36594fbd2179f853d8c48de15d78e772173d0936
anthonyray <dantho361@hotmail.com>
2018-05-24 15:20:29 +0200

    adds further testing for volume up use case

--- tests/use_cases/test_volume_up_request_objects.py
+++ tests/use_cases/test_volume_up_request_objects.py
@@ -1,4 +1,5 @@
 from snipssonos.use_cases import request_objects as reqo
+from snipssonos.shared.request_object import InvalidRequestObject, ValidRequestObject
 
 def test_build_volume_up_request_object_without_params():
     req = reqo.VolumeUpRequestObject()
@@ -10,3 +11,55 @@ def test_build_volume_up_request_object_from_empty_dict():
     req = reqo.VolumeUpRequestObject.from_dict({})
 
     assert bool(req) is True
+
+def test_returns_invalid_request_object_for_wrong_params_dictionary():
+    req = reqo.VolumeUpRequestObject.from_dict({
+        'volume_increase': 'loud'
+    })
+
+    assert bool(req) is False
+    assert isinstance(req, InvalidRequestObject)
+
+def test_returns_invalid_request_object_for_out_of_range_params_dictionary1():
+    req = reqo.VolumeUpRequestObject.from_dict({
+        'volume_increase': 1234
+    })
+
+    assert bool(req) is False
+    assert isinstance(req, InvalidRequestObject)
+    assert req.has_errors() is True
+
+
+def test_returns_invalid_request_object_for_out_of_range_params_dictionary2():
+    req = reqo.VolumeUpRequestObject.from_dict({
+        'volume_increase': -123
+    })
+
+    assert bool(req) is False
+    assert isinstance(req, InvalidRequestObject)
+    assert req.has_errors() is True
+
+
+def test_returns_invalid_request_object_for_out_of_range_params():
+    req = reqo.VolumeUpRequestObject(volume_increase=1234)
+
+    assert bool(req) is False
+    assert isinstance(req, InvalidRequestObject)
+    assert req.has_errors() is True
+
+
+def test_returns_invalid_request_object_for_out_of_range_params():
+    req = reqo.VolumeUpRequestObject(volume_increase=-1234)
+
+    assert bool(req) is False
+    assert isinstance(req, InvalidRequestObject)
+    assert req.has_errors() is True
+
+
+def test_returns_invalid_request_object_for_wrong_params():
+    req = reqo.VolumeUpRequestObject(volume_increase="louder")
+
+    assert bool(req) is False
+    assert isinstance(req, InvalidRequestObject)
+    assert req.has_errors() is True
+
