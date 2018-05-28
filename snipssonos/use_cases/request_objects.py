2d5cbdd6abe49e79eb8df669b1ab254bd7749f1a
anthonyray <dantho361@hotmail.com>
2018-05-28 18:44:45 +0200

    adding use case code for volume down

--- snipssonos/use_cases/request_objects.py
+++ snipssonos/use_cases/request_objects.py
@@ -23,3 +23,28 @@ class VolumeUpRequestObject(ValidRequestObject):
         return cls(
             volume_increase=a_dictionary.get('volume_increase', None)
         )
+
+
+class VolumeDownRequestObject(ValidRequestObject):
+    def __init__(self,  volume_decrease=None):
+        self.volume_decrease = volume_decrease
+
+    @classmethod
+    def from_dict(cls, a_dictionary):
+        invalid_request = InvalidRequestObject()
+
+        if 'volume_decrease' in a_dictionary and not isinstance(a_dictionary['volume_decrease'], int):
+            invalid_request.add_error('volume_decrease', 'must be an integer')
+
+        if 'volume_decrease' in a_dictionary and isinstance(a_dictionary['volume_decrease'], int) and a_dictionary['volume_decrease'] < 0:
+            invalid_request.add_error('volume_decrease', 'must be positive')
+
+        if 'volume_decrease' in a_dictionary and isinstance(a_dictionary['volume_decrease'], int) and a_dictionary['volume_decrease'] > 100:
+            invalid_request.add_error('volume_decrease', 'must be lower than 100')
+
+        if invalid_request.has_errors():
+            return invalid_request
+
+        return cls(
+            volume_decrease=a_dictionary.get('volume_decrease', None)
+        )
---
+++ snipssonos/use_cases/volume_down.py
@@ -0,0 +1,21 @@
+from snipssonos.shared.use_case import UseCase
+from snipssonos.shared.response_object import ResponseSuccess, ResponseFailure
+from snipssonos.exceptions import NoReachableDeviceException
+
+class VolumeDownUseCase(UseCase):
+    DEFAULT_VOLUME_DECREMENT = 10
+
+    def __init__(self, device_discovery_service, device_transport_control_service):
+        self.device_discovery_service = device_discovery_service
+        self.device_transport_control_service = device_transport_control_service
+
+
+    def process_request(self, request_object):
+        device = self.device_discovery_service.get()
+        if request_object.volume_increase:
+            device.increase_volume(request_object.volume_increase)
+            self.device_transport_control_service.volume_up(device)
+        else:
+            device.increase_volume(self.DEFAULT_VOLUME_DECREMENT)
+            self.device_transport_control_service.volume_up(device)
+        return ResponseSuccess()
