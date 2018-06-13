b61db199987b1e9f4f079663359147fa7cee9027
anthonyray <dantho361@hotmail.com>
2018-06-13 12:28:10 +0200

    adding play playlist sub use case

---
+++ snipssonos/shared/feedback.py
@@ -0,0 +1,1 @@
+TTS_GENERIC_ERROR = "Sorry, I couln't find any music with those parameters. Please try asking it a different way, or try with other music ! You can ask by song, artist, album, genre, playlist ..."
