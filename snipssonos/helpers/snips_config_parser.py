57a9ef3f7e010123a0da12bea266abf2764ed814
paufabregat <pau.fabregat.p@gmail.com>
2018-06-15 13:37:51 +0200

    Create config parser

---
+++ snipssonos/helpers/snips_config_parser.py
@@ -0,0 +1,20 @@
+import io
+import ConfigParser
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+
+
+class SnipsConfigParser(ConfigParser.SafeConfigParser):
+    def to_dict(self):
+        return {section: {option_name: option for option_name, option in self.items(section)} for section in
+                self.sections()}
+
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, ConfigParser.Error) as e:
+        return dict()
