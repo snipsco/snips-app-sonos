9a9e12a72c3f14df083fb8deb51b10f2226582e2
Anthony Reinette <dantho361@hotmail.com>
2018-06-15 11:43:05 +0200

    adds a generic short error feedback for the TTS

--- snipssonos/use_cases/play_playlist.py
+++ snipssonos/use_cases/play_playlist.py
@@ -1,6 +1,6 @@
 from snipssonos.shared.use_case import UseCase
 from snipssonos.shared.response_object import ResponseSuccess, ResponseFailure
-from snipssonos.shared.feedback import TTS_GENERIC_ERROR
+from snipssonos.shared.feedback import FR_TTS_GENERIC_ERROR, FR_TTS_PLAYING_PLAYLIST_TEMPLATE
 
 class PlayPlaylistUseCase(UseCase):
 
@@ -18,8 +18,8 @@ class PlayPlaylistUseCase(UseCase):
             if len(results_playlists):
                 first_playlist = results_playlists[0]
                 self.music_playback_service.play(device, first_playlist)
-                tts_feedback = "Playing the playlist : {}".format(first_playlist.name)
+                tts_feedback = FR_TTS_PLAYING_PLAYLIST_TEMPLATE.format(first_playlist.name)
             else:
-                return ResponseFailure.build_resource_error(TTS_GENERIC_ERROR)
+                return ResponseFailure.build_resource_error(FR_TTS_GENERIC_ERROR)
 
         return ResponseSuccess(feedback=tts_feedback)
