750573e0c2e6225df6b716f1bda7f8dce1b2a864
paufabregat <pau.fabregat.p@gmail.com>
2018-06-19 17:50:41 +0200

    Test fetch top artist

--- tests/services/spotify/test_music_custom_service.py
+++ tests/services/spotify/test_music_custom_service.py
@@ -1,3 +1,5 @@
+import mock
+
 from snipssonos.services.spotify.music_custom_service import SpotifyCustomService
 from tests.services.spotify.raw_responses import TOP_ARTISTS, EMPTY_ITEMS
 
@@ -16,4 +18,16 @@ def test_correct_parsing_of_tracks_with_empty_response():
     tracks = client.parse_artist_results(EMPTY_ITEMS)
 
     assert len(tracks) == 0
+
+@mock.patch('snipssonos.services.spotify.music_custom_service.SpotifyClient')
+def test_fetch_top_artists(mock_spotify_client):
+    mock_spotify_client_instance = mock_spotify_client.return_value
+    mock_spotify_client_instance.execute_query.return_value = TOP_ARTISTS
+
+    client = SpotifyCustomService("client_id", "client_secret")
+    artists = client.fetch_top_artist()
+
+    assert len(artists) == 60
+    assert artists[0].name == "The Weeknd"
+    assert artists[0].uri == "spotify:artist:1Xyo4u8uXC1ZmMpatF05PJ"
 
