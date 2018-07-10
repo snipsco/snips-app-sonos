e86037ca2d56b5662d8a2aefae6cba455da3e084
paufabregat <pau.fabregat.p@gmail.com>
2018-07-10 11:31:24 +0200

    Entities can handle artist name

--- snipssonos/entities/album.py
+++ snipssonos/entities/album.py
@@ -2,15 +2,17 @@ from snipssonos.entities.entities import Entity
 
 
 class Album(object):
-    def __init__(self, uri, name):
+    def __init__(self, uri, name, artist_name=None):
         self.uri = uri
         self.name = name
+        self.artist_name = artist_name
 
     @classmethod
     def from_dict(cls, a_dict):
         album = cls(
             uri=a_dict['uri'],
-            name=a_dict['name']
+            name=a_dict['name'],
+            artist_name=a_dict['artist_name']
         )
 
         return album
--- snipssonos/entities/track.py
+++ snipssonos/entities/track.py
@@ -2,18 +2,23 @@ from snipssonos.entities.entities import Entity
 
 
 class Track(object):
-    def __init__(self, uri, name=None):
+    def __init__(self, uri, name=None, artist_name=None):
         self.uri = uri
         self.name = name
+        self.artist_name = artist_name
 
     @classmethod
     def from_dict(cls, a_dict):
         name = None
+        artist_name = None
         if 'name' in a_dict:
             name = a_dict['name']
+        if 'artist_name' in a_dict:
+            artist_name = a_dict['artist_name']
         track = cls(
             uri=a_dict['uri'],
-            name=name
+            name=name,
+            artist_name=artist_name
         )
 
         return track
