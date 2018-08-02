2d38d339dd997707e037a32da9d9812b92ea9391
Anthony Reinette <dantho361@hotmail.com>
2018-08-02 18:42:50 +0200

    turning every string into unicode

--- snipssonos/services/feedback/feedback_messages.py
+++ snipssonos/services/feedback/feedback_messages.py
@@ -1,36 +1,36 @@
 # coding:utf-8
 
-EN_TTS_GENERIC_ERROR = "Sorry, I couln't find any music with those parameters. Please try asking it a different way, or try with other music !"
-FR_TTS_GENERIC_ERROR = "Désolé, je ne trouve pas de musique correspondant à votre recherche. Veuillez formuler votre requête différemment."
+EN_TTS_GENERIC_ERROR = u"Sorry, I couln't find any music with those parameters. Please try asking it a different way, or try with other music !"
+FR_TTS_GENERIC_ERROR = u"Désolé, je ne trouve pas de musique correspondant à votre recherche. Veuillez formuler votre requête différemment."
 
-EN_TTS_SHORT_ERROR = "Sorry, I did not understand that."
-FR_TTS_SHORT_ERROR = "Désolé, je n'ai pas compris."
+EN_TTS_SHORT_ERROR = u"Sorry, I did not understand that."
+FR_TTS_SHORT_ERROR = u"Désolé, je n'ai pas compris."
 
 # PlayListUseCase
-EN_TTS_PLAYING_PLAYLIST_TEMPLATE = "Playing the playlist {}"
-FR_TTS_PLAYING_PLAYLIST_TEMPLATE = "Lecture de la playlist : {}"
+EN_TTS_PLAYING_PLAYLIST_TEMPLATE = u"Playing the playlist {}"
+FR_TTS_PLAYING_PLAYLIST_TEMPLATE = u"Lecture de la playlist : {}"
 
 # TrackUseCase
-EN_TTS_PLAYING_TRACK_TEMPLATE = "Playing {} by artist {}"
-FR_TTS_PLAYING_TRACK_TEMPLATE = "Lecture de : {} par {}"
+EN_TTS_PLAYING_TRACK_TEMPLATE = u"Playing {} by artist {}"
+FR_TTS_PLAYING_TRACK_TEMPLATE = u"Lecture de : {} par {}"
 
 # ArtistUseCase
-EN_TTS_PLAYING_ARTIST_TEMPLATE = "Playing music by artist {}"
-FR_TTS_PLAYING_ARTIST_TEMPLATE = "Lecture de la musique par {}"
+EN_TTS_PLAYING_ARTIST_TEMPLATE = u"Playing music by artist {}"
+FR_TTS_PLAYING_ARTIST_TEMPLATE = u"Lecture de la musique par {}"
 
 # AlbumUseCase
-EN_TTS_PLAYING_ALBUM_TEMPLATE = "Playing the album {} by artist {}"
-FR_TTS_PLAYING_ALBUM_TEMPLATE = "Lecture de l'album {} par {}"
+EN_TTS_PLAYING_ALBUM_TEMPLATE = u"Playing the album {} by artist {}"
+FR_TTS_PLAYING_ALBUM_TEMPLATE = u"Lecture de l'album {} par {}"
 
 # Device Discovery
-FR_TTS_DEVICE_DISCOVERY_SERVICE_UNREACHABLE = "Le service de découverte d'équipement n'est pas disponible ... Veuillez consulter la documentation pour plus d'information"
-EN_TTS_DEVICE_DISCOVERY_SERVICE_UNREACHABLE = "The Device Discovery service is unreachable ... Please read our documentation for more information"
+FR_TTS_DEVICE_DISCOVERY_SERVICE_UNREACHABLE = u"Le service de découverte d'équipement n'est pas disponible ... Veuillez consulter la documentation pour plus d'information"
+EN_TTS_DEVICE_DISCOVERY_SERVICE_UNREACHABLE = u"The Device Discovery service is unreachable ... Please read our documentation for more information"
 
 # Get Info use case
-EN_TTS_TRACK_INFO = "This is {} by {}"
-FR_TTS_TRACK_INFO = "C'est {} par {}"
+EN_TTS_TRACK_INFO = u"This is {} by {}"
+FR_TTS_TRACK_INFO = u"C'est {} par {}"
 
 # TODO complete message and translation
-EN_TTS_TRACK_INFO_NO_TRACKS_ERROR = "There is nothing playing right now. If you want to play any music just ask for it"
-FR_TTS_TRACK_INFO_NO_TRACKS_ERROR = "Il n'y a pas de musique pour le moment. Si vous voulez mettre de la musique vous avez juste à le demander"
+EN_TTS_TRACK_INFO_NO_TRACKS_ERROR = u"There is nothing playing right now. If you want to play any music just ask for it"
+FR_TTS_TRACK_INFO_NO_TRACKS_ERROR = u"Il n'y a pas de musique pour le moment. Si vous voulez mettre de la musique vous avez juste à le demander"
 
